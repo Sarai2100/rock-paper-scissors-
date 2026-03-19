0e17c7effe18d873654a8ac1145fe633eaa5268b
Sara <sara.miromano@gmail.com>
2026-03-19 19:32:47 -0400

    Pseudocode added

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log("hello")
+
+// Pseudocode -- 
+// we need to let the player know what game are we playing
+
+//then we are going to let them chose what  item they are going to chose rock paper or scissors 
+// then we are going to make the computer chose a random item as well.
+//if the player chose rock and the computer chooses scissors the player wins
+//if the player chose paper and the computer chooses scissors the player lose. 
+// if the player chose scissors and the computer chooses scissors they are even.
+
