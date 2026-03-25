42228db3b4acb45916f5e29c73d46855177c6fda
Sara <sara.miromano@gmail.com>
2026-03-25 16:58:36 -0400

    Add 5 round game loop with score tracking and  winner declaration

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("hello")
+
 
 // Pseudocode -- 
 // we need to let the player know what game are we playing
@@ -8,12 +8,13 @@ console.log("hello")
 //if the player chose rock and the computer chooses scissors the player wins
 //if the player chose paper and the computer chooses scissors the player loses. 
 // if the player chose scissors and the computer chooses scissors they are tie
+let humanScore = 0;
+let computerScore = 0;
 
-
-let message = prompt("Hi! Welcome to rock paper scissors, What would you chose?").toLowerCase();
+// let message = prompt("Hi! Welcome to rock paper scissors, What would you chose?").toLowerCase();
 // alert (results)
     //  COMPUTER GETS RANDOM CHOICE
-function getRandomInt () {
+function getComputerChoice () {
     let randomNumber = Math.floor(Math.random () * 3)
         if (randomNumber == 0) {
         return "rock"
@@ -26,30 +27,64 @@ function getRandomInt () {
     }
 }
 
-function results() {
-    let computerChoice = getRandomInt()
-    let playerChoice = message
-    console.log(computerChoice)
-    console.log(playerChoice)
-    if (computerChoice == "scissors" && playerChoice == "rock") {
-        alert("You Win!") 
-    }  else if ( computerChoice == "rock" && playerChoice == "scissors") {
-        alert("You Lose!")
-    } else if (computerChoice == "paper" && playerChoice == "scissors" ) {
-         alert("You Win!")
-    }  else if (computerChoice == "scissors" && playerChoice == "paper") {
-        alert(" You Lose!")
-    } else if (computerChoice == "rock" && playerChoice == "paper") {
-        alert("You Win!")
-    } else if (computerChoice == "paper" && playerChoice == "rock") {
-        alert("You Lose!")
-    } else if (computerChoice == "rock" && playerChoice == "rock") {
-        alert("Its a tie!")
-    } else if (computerChoice == "paper" && playerChoice == "paper") {
-        alert("Its a tie!")
-    } else if (computerChoice == "scissors" && playerChoice == "scissors") {
-        alert("Its a tie!")
-    }
+
+
+function getHumanChoice() {
+    let message = prompt("Hi! Welcome to rock paper scissors, What would you chose?").toLowerCase();
+    return message
 }
 
-results ()
+
+
+
+function playRound(humanChoice, computerChoice) {
+    // let computerChoice = getComputerChoice()
+    // let humanChoice = message
+
+    if (computerChoice == "scissors" && humanChoice == "rock") {
+        alert("You Win!")
+        humanScore++ 
+    }  else if ( computerChoice == "rock" && humanChoice == "scissors") {
+        alert("You Lose!")
+        computerScore++
+    } else if (computerChoice == "paper" && humanChoice == "scissors" ) {
+         alert("You Win!")
+         humanScore++ 
+    }  else if (computerChoice == "scissors" && humanChoice == "paper") {
+        alert(" You Lose!")
+        computerScore++
+    } else if (computerChoice == "rock" && humanChoice == "paper") {
+        alert("You Win!")
+        humanScore++ 
+    } else if (computerChoice == "paper" && humanChoice == "rock") {
+        alert("You Lose!")
+        computerScore++
+    } else if (computerChoice == "rock" && humanChoice == "rock") {
+        alert("Its a tie!")
+    } else if (computerChoice == "paper" && humanChoice == "paper") {
+        alert("Its a tie!")
+    } else if (computerChoice == "scissors" && humanChoice == "scissors") {
+        alert("Its a tie!")
+    }
+    
+}
+
+
+function playGame() {
+    for (let i = 0; i < 5; i++){
+        let humanChoice = getHumanChoice()
+        let computerChoice = getComputerChoice()
+        playRound(humanChoice, computerChoice)
+    }
+    if( humanScore > computerScore){
+        console.log("You win the Game!")
+
+    } else if (computerScore > humanScore) {
+        console.log("You lose the game!")
+    } else {
+        console.log("It's a tie!")
+    }
+
+        
+}
+playGame()
